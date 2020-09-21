dc0b3ae99d409e7ff212d89b7fe64e7abaf4ee6b
Abner Zheng <abnerzheng@gmail.com>
2020-09-21 22:49:36 +0800

    ECO-658: make sure stored version faucet contract exists on blockchain each time when we call faucet contract.
    Signed-off-by: Abner Zheng <abnerzheng@gmail.com>

--- packages/server/src/StoredFaucetService.ts
+++ packages/server/src/StoredFaucetService.ts
@@ -10,6 +10,7 @@ import {
 import { ByteArray, SignKeyPair } from 'tweetnacl-ts';
 import { CallFaucet, StoredFaucet } from './lib/Contracts';
 import DeployService from './services/DeployService';
+import { DeployInfo } from 'casperlabs-grpc/io/casperlabs/casper/consensus/info_pb';
 
 // based on execution-engine/contracts/explorer/faucet-stored/src/main.rs
 const CONTRACT_NAME = 'faucet';
@@ -33,24 +34,68 @@ export class StoredFaucetService {
     this.periodCheckState();
   }
 
+  async deployStoredVersionFaucet() {
+    const deploy = this.faucetContract.deploy(
+      StoredFaucet.args(),
+      this.paymentAmount
+    );
+    await this.deployService.deploy(deploy);
+    this.deployHash = deploy.getDeployHash_asU8();
+    return deploy.getDeployHash_asU8();
+  }
+
   async callStoredFaucet(accountPublicKeyHash: ByteArray): Promise<DeployHash> {
+    let justDeployed = false;
+    // if the current Clarity instance haven't deploy stored version faucet,
     if (!this.storedFaucetFinalized && !this.deployHash) {
+      // check whether Clarity has deployed in the blockchain before,
       const state = await this.checkState();
       if (state) {
+        // this could happen when Clarity get restarted, we shouldn't deploy new stored version faucet.
         this.storedFaucetFinalized = true;
       } else {
-        const deploy = this.faucetContract.deploy(
-          StoredFaucet.args(),
-          this.paymentAmount
-        );
-        await this.deployService.deploy(deploy);
-        this.deployHash = deploy.getDeployHash_asU8();
+        await this.deployStoredVersionFaucet();
+        justDeployed = true;
       }
     }
 
     const dependencies = [];
+    // make sure stored version faucet contract exists on the blockchain.
     if (this.deployHash) {
-      dependencies.push(this.deployHash);
+      if (!justDeployed) {
+        try {
+          const deploy = await this.casperService.getDeployInfo(
+            this.deployHash
+          );
+
+          const needRedeploy =
+            deploy.getStatus()?.getState() !== DeployInfo.State.FINALIZED &&
+            deploy.getStatus()?.getState() !== DeployInfo.State.PROCESSED;
+
+          const deployTimestamp = deploy
+            .getDeploy()!
+            .getHeader()!
+            .getTimestamp();
+
+          // if more than two hours passed, and the deploy still don't get processed or get finalized,
+          // we should deploy a new one.
+          if (
+            deployTimestamp &&
+            Date.now() - deployTimestamp > 2 * 60 * 60 * 1000 &&
+            needRedeploy
+          ) {
+            const deployHash = await this.deployStoredVersionFaucet();
+            dependencies.push(deployHash);
+          }
+        } catch {
+          // throw error if no deploy found
+          const deployHash = await this.deployStoredVersionFaucet();
+          dependencies.push(deployHash);
+        }
+      } else {
+        // if we just deployed the contract, set dependencies directly
+        dependencies.push(this.deployHash);
+      }
     }
     const deployByName = DeployUtil.makeDeploy(
       CallFaucet.args(accountPublicKeyHash, this.transferAmount),
