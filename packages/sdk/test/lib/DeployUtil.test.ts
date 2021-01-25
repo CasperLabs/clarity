9e5d74b41a4d2ad4dd22c3311e618fb59b77a7d2
Abner Zheng <abnerzheng@gmail.com>
2021-01-26 00:57:06 +0800

    fix bug

--- packages/sdk/test/lib/DeployUtil.test.ts
+++ packages/sdk/test/lib/DeployUtil.test.ts
@@ -4,9 +4,17 @@ import { Keys } from '../../src/lib';
 import { TypedJSON } from 'typedjson';
 
 describe('DeployUtil', () => {
-  it('should stringify/parse DeployHeader correctly', function() {
+  it('should stringify/parse DeployHeader correctly', function () {
     const ed25519Key = Keys.Ed25519.new();
-    const deployHeader = new DeployHeader(ed25519Key.publicKey, 123456, 654321, 10, Uint8Array.from(Array(32).fill(42)), [Uint8Array.from(Array(32).fill(2))], 'test-network');
+    const deployHeader = DeployHeader.newDeployHeader(
+      ed25519Key.publicKey,
+      123456,
+      654321,
+      10,
+      Uint8Array.from(Array(32).fill(42)),
+      [Uint8Array.from(Array(32).fill(2))],
+      'test-network'
+    );
     const serializer = new TypedJSON(DeployHeader);
     const json = serializer.stringify(deployHeader);
     const deployHeader1 = serializer.parse(json);
